4afcb9250768e09952c27be6396aef7322c22d75
Dao Tan Phat <daotanphat2143@gmail.com>
2025-02-02 01:27:06 +0700

    Response DTO

---
+++ Backend/BusinessObjects/Dtos/ResponseApiDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Dtos
+{
+	public class ResponseApiDto<T>
+	{
+		public string Status { get; set; }
+		public string Message { get; set; }
+		public T Data { get; set; }
+
+		public ResponseApiDto(string status, string message, T data)
+		{
+			Status = status;
+			Message = message;
+			Data = data;
+		}
+	}
+}
