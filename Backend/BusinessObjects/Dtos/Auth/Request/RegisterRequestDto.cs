53842122180f2ed01d19a73fa3ce31a8a7f97a72
Dao Tan Phat <daotanphat2143@gmail.com>
2025-02-01 22:34:18 +0700

    Auth DTO

---
+++ Backend/BusinessObjects/Dtos/Auth/Request/RegisterRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Dtos.Auth.Request
+{
+	public class RegisterRequestDto
+	{
+		[Required(ErrorMessage = "Username can not be null")]
+		public string? UserName { get; set; }
+		[Required]
+		[EmailAddress]
+		public string? Email { get; set; }
+		[Required]
+		public string? Password { get; set; }
+	}
+}
