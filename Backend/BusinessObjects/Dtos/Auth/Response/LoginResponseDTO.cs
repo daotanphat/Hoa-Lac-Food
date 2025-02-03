e4f43b7c2c816155e54169dad0ff9218fa72ac90
Dao Tan Phat <daotanphat2143@gmail.com>
2025-02-03 23:15:06 +0700

    Login DTO

---
+++ Backend/BusinessObjects/Dtos/Auth/Response/LoginResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects.Dtos.Auth.Response
+{
+	public class LoginResponseDTO
+	{
+		public string UserName { get; set; }
+		public string Email { get; set; }
+		public string Token { get; set; }
+	}
+}
