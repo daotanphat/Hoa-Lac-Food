9071ba5f9366a87635e39f902c4d32ee5efac490
Dao Tan Phat <daotanphat2143@gmail.com>
2025-02-08 16:44:03 +0700

    Auth Service

---
+++ Backend/FoodDeliveryAPI/Service/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using BusinessObjects;
+using BusinessObjects.Dtos.Auth.Request;
+using BusinessObjects.Dtos.Auth.Response;
+
+namespace FoodDeliveryAPI.Service
+{
+	public interface IAuthService
+	{
+		Task<LoginResponseDTO> Login(LoginRequestDto request);
+	}
+}
---
+++ Backend/FoodDeliveryAPI/Service/Implement/AuthServiceImpl.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using BusinessObjects;
+using BusinessObjects.Dtos.Auth.Request;
+using BusinessObjects.Dtos.Auth.Response;
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodDeliveryAPI.Service.Implement
+{
+	public class AuthServiceImpl : IAuthService
+	{
+		private readonly UserManager<AppUser> _userManager;
+		private readonly ITokenService _tokenService;
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly ApplicationDBContext _context;
+		public AuthServiceImpl(UserManager<AppUser> userManager, ITokenService tokenService,
+			SignInManager<AppUser> signInManager, ApplicationDBContext context)
+		{
+			_userManager = userManager;
+			_tokenService = tokenService;
+			_signInManager = signInManager;
+			_context = context;
+		}
+		public async Task<LoginResponseDTO> Login(LoginRequestDto request)
+		{
+			var user = await _userManager.FindByNameAsync(request.UserName);
+
+			if (user == null) return null;
+
+			var result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+			if (!result.Succeeded) return null;
+
+			var loginResponse = new LoginResponseDTO
+			{
+				UserName = user.UserName,
+				Email = user.Email,
+				Token = _tokenService.CreateToken(user)
+			};
+
+			return loginResponse;
+		}
+	}
+}
