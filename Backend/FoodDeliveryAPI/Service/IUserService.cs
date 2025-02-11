870554dbcec6fee94a885402f149509e1f138068
Dao Tan Phat <daotanphat2143@gmail.com>
2025-02-11 15:14:48 +0700

    Get user from token

---
+++ Backend/FoodDeliveryAPI/Service/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using BusinessObjects;
+
+namespace FoodDeliveryAPI.Service
+{
+	public interface IUserService
+	{
+		Task<AppUser> GetUserFromToken(string token);
+	}
+}
---
+++ Backend/FoodDeliveryAPI/Service/Implement/UserServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using BusinessObjects;
+using Microsoft.AspNetCore.Identity;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace FoodDeliveryAPI.Service.Implement
+{
+	public class UserServiceImpl : IUserService
+	{
+		private readonly ITokenService _tokenService;
+		private readonly UserManager<AppUser> _userManager;
+		public UserServiceImpl(ITokenService tokenService, UserManager<AppUser> userManager)
+		{
+			_tokenService = tokenService;
+			_userManager = userManager;
+		}
+		public async Task<AppUser> GetUserFromToken(string token)
+		{
+			var principal = _tokenService.GetPrincipalFromToken(token);
+			if (principal == null) return null;
+
+			var email = principal.FindFirst(JwtRegisteredClaimNames.Email)?.Value;
+			if (string.IsNullOrEmpty(email)) return null;
+
+			return await _userManager.FindByEmailAsync(email);
+		}
+	}
+}
