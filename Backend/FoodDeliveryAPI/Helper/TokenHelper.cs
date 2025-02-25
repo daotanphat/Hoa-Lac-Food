bb21c715112d8fd8292278c3cc291dbc314ac785
Dao Tan Phat <daotanphat2143@gmail.com>
2025-02-26 00:16:00 +0700

    Extract Token From Header

---
+++ Backend/FoodDeliveryAPI/Helper/TokenHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace FoodDeliveryAPI.Helper
+{
+	public static class TokenHelper
+	{
+		public static string ExtractBearerToken(string header)
+		{
+			if (string.IsNullOrEmpty(header) || !header.StartsWith("Bearer "))
+				throw new UnauthorizedAccessException("Invalid or missing authorization token!");
+
+			return header["Bearer ".Length..].Trim();
+		}
+	}
+}
