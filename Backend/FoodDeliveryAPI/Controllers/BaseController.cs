3c446fb3cf7a2d6f60eff9a05d21d462c7c5e4e9
Dao Tan Phat <daotanphat2143@gmail.com>
2025-03-02 13:38:56 +0700

    Base Controller To Get Authenticated User

---
+++ Backend/FoodDeliveryAPI/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessObjects;
+using FoodDeliveryAPI.Helper;
+using FoodDeliveryAPI.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodDeliveryAPI.Controllers
+{
+	[Authorize]
+	public class BaseController : ControllerBase
+	{
+		private readonly IUserService _userService;
+		public BaseController(IUserService userService)
+		{
+			_userService = userService;
+		}
+		protected async Task<AppUser> GetAuthenticatedUser(string header)
+		{
+			var token = TokenHelper.ExtractBearerToken(header);
+			return await _userService.GetUserFromToken(token);
+		}
+	}
+}
