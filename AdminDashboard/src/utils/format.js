5126e680aede00f33d92bfb80e11a1c0766c2162
Dao Tan Phat <daotanphat2143@gmail.com>
2025-03-14 14:28:30 +0700

    Add formart datetime

--- AdminDashboard/src/utils/format.js
+++ AdminDashboard/src/utils/format.js
@@ -4,3 +4,15 @@ export const formatPrice = (price) => {
         currency: "VND",
     }).format(price);
 };
+
+export const formatDateTime = (date) => {
+    return new Intl.DateTimeFormat("en-US", {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: true
+    }).format(new Date(date));
+};
